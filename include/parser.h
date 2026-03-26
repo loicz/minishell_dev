48b8d0312f7f1f5069294aec794a78c90ef56430
lozhao <lozhao@student.42.fr>
2026-03-26 18:05:24 +0100

    add the very first draft of parser.h

---
+++ include/parser.h
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lozhao <lozhao@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/26 16:52:54 by lozhao            #+#    #+#             */
+/*   Updated: 2026/03/26 18:04:03 by lozhao           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER_H
+# define PARSER_H
+
+# include <stddef.h>
+# include "lexer.h"
+
+typedef enum e_redir_type
+{
+	R_IN,
+	R_OUT,
+	R_APPEND,
+	R_HEREDOC
+}	t_redir_type;
+
+typedef struct s_redir
+{
+	t_redir_type	type;
+	t_word			*target;
+	int				heredoc_fd;
+	bool			heredoc_expand;
+	struct s_redir	*next;
+}	t_redir;
+
+typedef struct s_cmd
+{
+	t_word	**argv_words;
+	size_t	argc;
+	t_redir	*redirs;
+}	t_cmd;
+
+typedef enum e_ast_type
+{
+	AST_CMD,
+	AST_PIPE,
+	AST_AND,
+	AST_OR,
+	AST_SUBSHELL
+}	t_ast_type;
+
+typedef struct s_ast
+{
+	t_ast_type			type;
+	union u_ast_data
+	{
+		t_cmd			cmd;
+		struct s_ast_bin
+		{
+			t_ast		*left;
+			t_ast		*right;
+		}bin;
+		struct s_ast	*child;
+	}	u;
+}	t_ast;
+
+typedef struct s_parser
+{
+	t_token	*cur;
+}	t_parser;
+
+//part of principal functions that I will use, there will be more than this
+t_ast	*ms_parse_tokens(t_token *tokens);
+t_ast	*ms_parse_simple_command(t_parser *ps);
+t_ast	*ms_parse_pipeline(t_parser *ps);
+t_ast	*ms_parse_logical_bonus(t_parser *ps);
+
+#endif
