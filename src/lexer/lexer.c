22b5ad672ef0351c8ee1f8b72842a3009196402c
Yihuan Zhang <yihzhang@f2r8s14.clusters.42paris.fr>
2026-04-08 16:15:09 +0200

    lexer_word consider the case: -1

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: yihzhang <yihzhang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/25 17:06:00 by yihzhang          #+#    #+#             */
-/*   Updated: 2026/03/30 20:26:38 by yihzhang         ###   ########.fr       */
+/*   Updated: 2026/04/08 15:48:10 by yihzhang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,9 +74,10 @@ void	token_add_back(t_token **list, t_token *new_node)
 	new_node->prev = tmp;
 }
 
-void	lex_input(char *line, t_token **list)
+int	lex_input(char *line, t_token **list)
 {
 	int	i;
+	int	next_i;
 
 	i = 0;
 	while (line[i])
@@ -86,8 +87,12 @@ void	lex_input(char *line, t_token **list)
 		if (line[i] == '\0')
 			break ;
 		if (is_op(line, i))
-			i = handle_operator(line, i, list);
+			next_i = handle_operator(line, i, list);
 		else
-			i = handle_words(line, i, list);
+			next_i = handle_words(line, i, list);
+		if (next_i < 0)
+			return (0);
+		i = next_i;
 	}
+	return (1);
 }
