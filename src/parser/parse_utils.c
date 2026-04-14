1071ce1630301e94a71ebbec54e5237187119135
lozhao <lozhao@student.42.fr>
2026-04-14 18:17:00 +0200

    finished parser utils file

--- src/parser/parse_utils.c
+++ src/parser/parse_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parse_utils_1.c                                    :+:      :+:    :+:   */
+/*   parse_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lozhao <lozhao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 11:38:53 by lozhao            #+#    #+#             */
-/*   Updated: 2026/04/14 12:22:01 by lozhao           ###   ########.fr       */
+/*   Updated: 2026/04/14 16:21:14 by lozhao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,16 @@ bool	ms_tok_starts_redir(t_token *tok)
 			|| tok->type == TOK_REDIR_OUT
 			|| tok->type == TOK_APPEND
 			|| tok->type == TOK_HEREDOC));
+}
+
+bool	ms_tok_starts_simple_item(t_token *tok)
+{
+	return ((tok && tok->type == TOK_WORD)
+			|| ms_tok_starts_redir(tok));
+}
+
+bool	ms_tok_starts_primary(t_token *tok)
+{
+	return ((tok && tok->type == TOK_LPAREN)
+			|| ms_tok_starts_simple_item(tok));
 }
